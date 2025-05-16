fe9ad93eb903b34fa29ddb43ee7874bb5dbbd2e0
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-05-16 18:12:45 -0300

    Godot Demo Project

---
+++ demo/main.gd
@@ -0,0 +1,22 @@
+@tool
+extends Node
+
+const dev_world_addr = "0x07cb912d0029e3799c4b8f2253b21481b2ec814c5daf72de75164ca82e7c42a5"
+const dev_actions_addr = "0x00a92391c5bcde7af4bad5fd0fff3834395b1ab8055a9abb8387c0e050a34edf"
+
+@onready var dojo:DojoC = DojoC.new()
+
+@onready var input_controller: TextEdit = $HBoxContainer/WorldConfig/InputController
+@onready var input_world_addr: TextEdit = $HBoxContainer/WorldConfig/InputWorldAddr
+@onready var status: Label = $HBoxContainer/WorldConfig/Status
+
+func _on_button_pressed() -> void:
+	if input_world_addr.text.is_empty():
+		input_world_addr.text = dev_world_addr
+	status.text = dojo.create_client(input_world_addr.text)
+	print(dojo.is_enabled)
+	
+func _on_connect_controller_pressed() -> void:
+	if input_controller.text.is_empty():
+		input_controller.text = dev_actions_addr
+	dojo.controller_connect(input_controller.text)
