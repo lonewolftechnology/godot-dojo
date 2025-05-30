43584befc41d950261e1475539a9c45812bbdc7b
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-05-30 12:40:28 -0300

    Updated demo for working spawning events

--- demo/main.gd
+++ demo/main.gd
@@ -5,17 +5,26 @@ const dev_world_addr = "0x07cb912d0029e3799c4b8f2253b21481b2ec814c5daf72de75164c
 const dev_actions_addr = "0x00a92391c5bcde7af4bad5fd0fff3834395b1ab8055a9abb8387c0e050a34edf"
 
 @onready var dojo:DojoC = DojoC.new()
-@onready var spawn_output: Label = $HBoxContainer/WorldConfig/SpawnOutput
 
-@onready var input_controller: TextEdit = $HBoxContainer/WorldConfig/InputController
-@onready var input_world_addr: TextEdit = $HBoxContainer/WorldConfig/InputWorldAddr
-@onready var status: Label = $HBoxContainer/WorldConfig/Status
+@onready var status: Label = $"TabContainer/WIP tests/Status"
+@onready var input_world_addr: TextEdit = $"TabContainer/WIP tests/InputWorldAddr"
+@onready var input_controller: TextEdit = $"TabContainer/WIP tests/InputController"
 
-@export_global_file("Scarb.toml") var dojo_starter_path:String
-@export_global_file("sozo") var sozo_path:String
+@onready var provider_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/ProviderStatus
+@onready var account_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/AccountStatus
+@onready var suscription_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/SuscriptionStatus
+@onready var spawn_output: RichTextLabel = $TabContainer/SpawnTest/ScrollContainer/SpawnOutput
+
+
 
 func _ready() -> void:
 	OS.set_environment("RUST_BACKTRACE", "full")
+	dojo.event_update.connect(_on_event_update)
+	dojo.account_status_updated.connect(update_status.bind(provider_status))
+	dojo.provider_status_updated.connect(update_status.bind(account_status))
+	dojo.subscription_status_updated.connect(update_event_subscription_status.bind(suscription_status))
+	
+
 
 func _on_button_pressed() -> void:
 	if input_world_addr.text.is_empty():
@@ -28,26 +37,22 @@ func _on_connect_controller_pressed() -> void:
 		input_controller.text = dev_actions_addr
 	dojo.controller_connect(input_controller.text)
 
-
 func _on_testing_pressed() -> void:
-	#dojo.call_deferred("testing")
 	dojo.testing()
-	await get_tree().create_timer(2).timeout
-	_on_spawn_pressed()
-	await get_tree().create_timer(2).timeout
-	dojo.output_message.any(
-		func(_msg):
-			print_rich("[color=Coral]value: ", _msg, " type: ", type_string(typeof(_msg)))
-	)
 
 func _on_spawn_pressed() -> void:
-	var args:Array = [
-		"execute",
-		"dojo_starter-actions",
-		"spawn",
-		"--manifest-path",
-		dojo_starter_path
-	]
-	var out:Array = []
-	OS.execute(sozo_path,args,out,true)
-	out.any(func(c): spawn_output.text += c)
+	dojo.spawn()
+
+func _on_event_update(_msg:String):
+	#_msg.any(func(c): spawn_output.text += c)
+	spawn_output.append_text(_msg)
+	spawn_output.newline()
+
+func update_event_subscription_status(_value:bool, _event:String, _status_node:HBoxContainer):
+	update_status(_value, _status_node)
+
+func update_status(_value:bool, _status_node:HBoxContainer):
+	if _value:
+		_status_node.get_node("Status").color = Color.GREEN
+	else:
+		_status_node.get_node("Status").color = Color.RED
