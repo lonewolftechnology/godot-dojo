84909ef4444883e4a708a67d73dfa52e5711341f
Rodrigo Diaz Figueroa <diazfrodrigo@hotmail.com>
2025-06-09 17:07:41 -0300

    fixed missing last changes parallax

--- demo/player.gd
+++ demo/player.gd
@@ -3,4 +3,5 @@ extends Node2D
 @onready var label: Label = $Label
 
 func _process(delta: float) -> void:
-	$Label.text = "%dx%d"%[roundi(position.x),roundi(position.y)]
+	var pos := position / Vector2(64,64)
+	$Label.text = "%dx%d"%[roundi(pos.x),roundi(pos.y)]
