803564e428e3dcbe8a3ca6ae210cbd0d7f48554f
Rodrigo Diaz Figueroa <diazfrodrigo@hotmail.com>
2025-06-09 15:05:33 -0300

    minor cleanup demo project

--- demo/main.gd
+++ demo/main.gd
@@ -1,29 +1,38 @@
 extends Node
 
+const STEP_SIZE := Vector2(64,64)
+
 const dev_world_addr = "0x07cb912d0029e3799c4b8f2253b21481b2ec814c5daf72de75164ca82e7c42a5"
 const dev_actions_addr = "0x00a92391c5bcde7af4bad5fd0fff3834395b1ab8055a9abb8387c0e050a34edf"
-#const katana_addr = "0x4b4154414e41"
+
+
 @onready var dojo:DojoC = DojoC.new()
 
-@onready var status: Label = $"TabContainer/WIP tests/Status"
-@onready var input_world_addr: TextEdit = $"TabContainer/WIP tests/InputWorldAddr"
-@onready var input_controller: TextEdit = $"TabContainer/WIP tests/InputController"
+@onready var status: Label = %Status
+@onready var input_world_addr: TextEdit = %InputWorldAddr
+@onready var input_controller: TextEdit = %InputController
 
-@onready var provider_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/ProviderStatus
-@onready var account_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/AccountStatus
-@onready var suscription_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/SuscriptionStatus
-@onready var spawn_output: RichTextLabel = $TabContainer/SpawnTest/ScrollContainer/SpawnOutput
-@onready var controller_account_status: HBoxContainer = $TabContainer/SpawnTest/HBoxContainer3/ControllerAccountStatus
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var chat_box: TextEdit = $TabContainer/Chat/ChatBox
+@onready var provider_status: HBoxContainer = %ProviderStatus
+@onready var account_status: HBoxContainer = %AccountStatus
+@onready var suscription_status: HBoxContainer = %uscriptionStatus
+@onready var spawn_output: RichTextLabel = %SpawnOutput
+@onready var controller_account_status: HBoxContainer = %ontrollerAccountStatus
+@onready var sprite: Sprite2D = %Sprite2D
+@onready var chat_box: TextEdit = %ChatBox
+@onready var tabs: TabContainer = %Tabs
+
+@onready var button_toggle: Button = %ButtonToggle
 
 @export_global_file("*.json") var dojo_abi: String
+
+var count: int = 0
 
 func _ready() -> void:
 	OS.set_environment("RUST_BACKTRACE", "full")
 	OS.set_environment("RUST_LOG", "debug")
 	OS.set_environment("BROWSER", "chromium")
 	if Engine.is_editor_hint(): return
+	button_toggle.set_pressed(true)
 	dojo.event_update.connect(_on_event_update)
 	dojo.account_status_updated.connect(update_status.bind(provider_status))
 	dojo.provider_status_updated.connect(update_status.bind(account_status))
@@ -32,7 +41,6 @@ func _ready() -> void:
 	if not dojo_abi.is_empty() and FileAccess.file_exists(dojo_abi):
 		var json_file = FileAccess.get_file_as_string(dojo_abi)
 		var json = JSON.parse_string(json_file)
-		#json.parse(json_file)
 		var contracts = json['contracts']
 		for data in contracts:
 			print(data['systems'])
@@ -41,14 +49,6 @@ func _ready() -> void:
 				if abi_item['type'] == "interface" and abi_item['name'] == "dojo_starter::systems::actions::IActions":
 					var items = abi_item['items']
 					print(items)
-					#for item in items:
-						#if item['type'] == "function":
-							#if item['state_mutability'] == "external":
-								#print(item)
-								#print(item['name'])
-	
-	#await get_tree().create_timer(1).timeout
-	#print(testing.as_packed_array())
 	
 
 func callable_test(args:Array):
@@ -60,9 +60,7 @@ func callable_test(args:Array):
 		await get_tree().process_frame
 		await get_tree().process_frame
 		await get_tree().process_frame
-		sprite_2d.position = args[1]
-		#spawn_output.call_deferred("append_text", vector)
-		#spawn_output.call_deferred("newline")
+		sprite.position = args[1] * STEP_SIZE
 	prints("AAAAAAAAAAAAAA\n")
 
 func _on_button_pressed() -> void:
@@ -89,25 +87,21 @@ func _on_spawn_pressed() -> void:
 	dojo.spawn(false)
 
 func _on_event_update(_msg:String):
-	#_msg.any(func(c): spawn_output.text += c)
 	spawn_output.append_text(_msg)
 	spawn_output.newline()
 
 func update_event_subscription_status(_value:bool, _event:String, _status_node:HBoxContainer):
 	update_status(_value, _status_node)
 
-func update_status(_value:bool, _status_node:HBoxContainer):
-	if _value:
-		_status_node.get_node("Status").color = Color.GREEN
-	else:
-		_status_node.get_node("Status").color = Color.RED
+func update_status(_value:bool, _status_node):
+	if _status_node is DojoStatusIndicator:
+		_status_node.set_status(_value)
 
 
 func _on_move_pressed() -> void:
 	var random_direction = FieldElement.from_enum(randi_range(0,3))
 	dojo.move(random_direction,false)
 	
-var count: int = 0
 func _input(event: InputEvent) -> void:
 	var direction:FieldElement
 	if event.is_action_pressed("ui_up"):
@@ -125,3 +119,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action("ui_accept"):
 		if chat_box.visible and not chat_box.text.is_empty():
 			dojo.send_message(chat_box.text)
+
+
+func _on_button_toggle_toggled(toggled_on: bool) -> void:
+	tabs.visible = toggled_on
--- demo/ui/status_indicator.gd
+++ demo/ui/status_indicator.gd
@@ -1,2 +1,21 @@
+@tool
 class_name DojoStatusIndicator
 extends HBoxContainer
+
+const COLOR_TRUE := Color.GREEN
+const COLOR_FALSE := Color.RED
+
+@export var type_name : String = "Type" :
+	set(val):
+		type_name = val
+		if is_instance_valid($Type):
+			$Type.text = type_name
+
+@onready var status: ColorRect = $Status
+
+
+func set_status(value:bool) -> void:
+	if value:
+		status.color = COLOR_TRUE
+	else:
+		status.color = COLOR_FALSE
