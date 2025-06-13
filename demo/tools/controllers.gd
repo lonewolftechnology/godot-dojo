75ed4300e5ba537bb0db6cd1a84635bf074c334e
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-06-13 18:08:49 -0300

    minor refactor on demo

--- demo/tools/controllers.gd
+++ demo/tools/controllers.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@export var generic_entity_scene:PackedScene
+@export var player_entity_scene:PackedScene
+var player:PlayerEntity
+
+var camera:Camera2D:
+	get:
+		if player:
+			return player.camera
+		else:
+			return get_viewport().get_camera_2d()
+
 func find_user(id:String) -> GenericEntity:
 	var controller:Array = get_children().filter(
 		func(c): return c.id == id
@@ -17,3 +28,26 @@ func move_controller(id:String, vec:Vector2)->void:
 	if entity:
 		await get_tree().process_frame
 		entity.move(vec)
+
+func spawn_entity(_data:Dictionary, is_player:bool = false):
+	var new_entity:GenericEntity
+	if is_player:
+		new_entity = player_entity_scene.instantiate()
+		player = new_entity
+	else:
+		new_entity = generic_entity_scene.instantiate()
+	await get_tree().process_frame
+	add_child(new_entity)
+	new_entity.setup(_data)	
+
+func _unhandled_input(event: InputEvent) -> void:
+				
+	if event.is_action_pressed("zoom_in"):
+		camera.zoom += Vector2(0.1, 0.1)
+
+	if event.is_action_pressed("zoom_out"):
+		camera.zoom -= Vector2(0.1, 0.1)
+
+	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		camera.position -= event.relative * 1/camera.zoom
+		
