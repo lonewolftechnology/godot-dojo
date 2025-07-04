a5e708052a20dae42bc58b7bfab19bd4d65524f9
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-07-04 17:15:29 -0300

    add default config

--- include/dojo_c_gdextension.h
+++ include/dojo_c_gdextension.h
@@ -7,29 +7,30 @@
 
 #include "godot_cpp/classes/object.hpp"
 
-namespace godot {
+using namespace godot;
 
-    class DojoC : public Object {
-        GDCLASS(DojoC, Object);
+class DojoC : public Object
+{
+    GDCLASS(DojoC, Object);
 
-    protected:
-        static void _bind_methods();
-		static DojoC *singleton;
-        bool enabled;
+protected:
+    static void _bind_methods();
+    static DojoC* singleton;
+    bool enabled;
 
-    public:
-        void set_enabled(bool p_enabled){enabled=p_enabled;};
-        bool get_enabled(){return enabled;}
+public:
+    void set_enabled(bool p_enabled) { enabled = p_enabled; };
+    bool get_enabled() const { return enabled; }
 
-        static DojoC *get_singleton();
+    static DojoC* get_singleton() { return singleton; }
 
 
-        DojoC();
-        ~DojoC();
+    DojoC();
+    ~DojoC();
+    static void init_config(bool reset = false);
+    static bool set_setting(const String& setting, const Variant& value, const bool& force = false);
 
-        //void _process(double delta) override;
-    };
-
-}
+    //void _process(double delta) override;
+};
 
 #endif //DOJO_C_GDEXTENSION_H
--- src/dojo_c_gdextension.cpp
+++ src/dojo_c_gdextension.cpp
@@ -9,7 +9,6 @@
 #include "godot_cpp/core/class_db.hpp"
 #include "tools/logger.h"
 
-using namespace godot;
 DojoC* DojoC::singleton = nullptr;
 
 void DojoC::_bind_methods()
@@ -17,6 +16,8 @@ void DojoC::_bind_methods()
     ClassDB::bind_method(D_METHOD("set_enabled", "p_enabled"), &DojoC::set_enabled);
     ClassDB::bind_method(D_METHOD("get_enabled"), &DojoC::get_enabled);
 
+    ClassDB::bind_static_method("DojoC", D_METHOD("init_config", "reset"), &DojoC::init_config);
+    ClassDB::bind_static_method("DojoC", D_METHOD("init_setting", "setting", "value", "force"), &DojoC::set_setting);
     // ClassDB::bind_method(D_METHOD("controller_connect"), &DojoC::controller_connect);
     ADD_PROPERTY(PropertyInfo(Variant::BOOL, "is_enabled"), "set_enabled", "get_enabled");
 
@@ -24,30 +25,44 @@ void DojoC::_bind_methods()
 
 DojoC::DojoC()
 {
-    if (Engine::get_singleton()->is_editor_hint())
-    {
-        Logger::debug_extra("DojoC", "is running in editor mode");
-        ProjectSettings* settings = ProjectSettings::get_singleton();
-        if (settings->has_setting("dojo/rpc_url") == false)
-        {
-            settings->set_setting("dojo/rpc_url", "");
-            settings->set_initial_value("dojo/rpc_url", "http://localhost:5050");
-            settings->save();
-        }
-        return;
-    }
     singleton = this;
     enabled = true;
-}
-
-DojoC* DojoC::get_singleton()
-{
-    return singleton;
+    init_config();
 }
 
 DojoC::~DojoC()
 {
-    // Add your cleanup here.
     singleton = nullptr;
 
+}
+
+void DojoC::init_config(bool reset)
+{
+    if (Engine::get_singleton()->is_editor_hint())
+    {
+        Logger::debug_extra("DojoC", "is running in editor mode");
+        set_setting("dojo/config/katana/rpc_url", "http://localhost:5050", reset);
+        set_setting("dojo/config/fixed_point/64", 40, reset);
+        set_setting("dojo/config/fixed_point/128", 60, reset);
+        set_setting("dojo/config/fixed_point/256", 123, reset);
+    }
+}
+
+bool DojoC::set_setting(const String& setting, const Variant& value, const bool& force)
+{
+    ProjectSettings* settings = ProjectSettings::get_singleton();
+    if (force)
+    {
+        Logger::info("Forcing init of config", setting);
+    }
+    if (settings->has_setting(setting) == false || force)
+    {
+        settings->set_setting(setting, value);
+        settings->set_as_basic(setting, true);
+        settings->set_initial_value(setting, value);
+        Logger::info("Inited config", setting, "with value", value);
+        settings->save();
+        return true;
+    }
+    return false;
 }
