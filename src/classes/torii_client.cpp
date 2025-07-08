e83c77da92db6685bf8f99be03cb2ad61769af45
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-07-08 11:08:57 -0300

    update to 1.6.0.alpha.0

--- src/classes/torii_client.cpp
+++ src/classes/torii_client.cpp
@@ -155,14 +155,14 @@ Dictionary ToriiClient::get_world_metadata()
         return Logger::error_dict("Client not connected");
     }
 
-    DOJO::ResultWorldMetadata resMetadata = DOJO::client_metadata(client);
+    DOJO::ResultWorld resMetadata = DOJO::client_metadata(client);
 
-    if (resMetadata.tag == DOJO::ErrWorldMetadata)
+    if (resMetadata.tag == DOJO::ErrWorld)
     {
         return Logger::error_dict("Error al obtener metadatos: ", GET_DOJO_ERROR(resMetadata));
     }
 
-    DOJO::WorldMetadata metadata = GET_DOJO_OK(resMetadata);
+    DOJO::World metadata = GET_DOJO_OK(resMetadata);
     Dictionary result = {};
 
     TypedArray<Dictionary> models_array = ArrayDojo(metadata.models).get_value();
@@ -222,17 +222,26 @@ TypedArray<Dictionary> ToriiClient::get_controllers(const TypedArray<String>& ad
     }
     std::vector<dojo_bindings::FieldElement> contract_addresses = FieldElement::create_array(addresses);
     Logger::debug_extra("TORII CLIENT", "Address size: ", contract_addresses.size());
-    DOJO::ResultCArrayController resControllers = DOJO::client_controllers(
-        client, contract_addresses.data(), contract_addresses.size()
+
+    DOJO::COptionu32 coption = {};
+    coption.tag = DOJO::COptionu32_Tag::Noneu32;
+
+    DOJO::COptionc_char coption_addresses = {};
+    coption_addresses.tag = DOJO::COptionc_char_Tag::Nonec_char;
+
+    DOJO::FieldElement nulled = FieldElement::nulled();
+
+    DOJO::ResultPageController resControllers = DOJO::client_controllers(
+        client, &nulled, 0, String().utf8().get_data(), 0, coption, coption_addresses
     );
 
-    if (resControllers.tag == DOJO::ErrCArrayController)
+    if (resControllers.tag == DOJO::ErrPageController)
     {
         Logger::error("Error obtaining controllers: ", GET_DOJO_ERROR(resControllers));
         return {Logger::error_dict("Error obtaining controllers: ", GET_DOJO_ERROR(resControllers))};
     }
 
-    DOJO::CArrayController controllers = resControllers.ok;
+    DOJO::PageController controllers = GET_DOJO_OK(resControllers);
     TypedArray<Dictionary> result = ArrayDojo(controllers).get_value();
 
     Logger::success("Controllers obtained: ", String::num_int64(result.size()));
@@ -425,7 +434,7 @@ DOJO::Query ToriiClient::create_query_from_dict(const Dictionary& query_params)
     DOJO::Pagination pagination = {};
     Logger::info("Pagination Creation");
     Dictionary pagination_dict = query_params.get("pagination", {});
-    pagination.limit = query_params.get("limit", 10);
+    pagination.limit.some = query_params.get("limit", 10);
     String cursor = pagination_dict.get("cursor", "");
     if (cursor.is_empty())
     {
@@ -495,7 +504,7 @@ DOJO::Pagination ToriiClient::create_pagination_from_dict(const Dictionary& pagi
 {
     DOJO::Pagination pagination = {};
 
-    pagination.limit = pagination_params.get("limit", 10);
+    pagination.limit.some = pagination_params.get("limit", 10);
     pagination.cursor.tag = DOJO::COptionc_char_Tag::Nonec_char;
 
     String direction = pagination_params.get("direction", "forward");
--- src/variant/ty/dojo_array.cpp
+++ src/variant/ty/dojo_array.cpp
@@ -51,9 +51,9 @@ ArrayDojo::ArrayDojo(DOJO::CArrayEnumOption array)
     value = CArrayEnumOptionToVariant(array);
 }
 
-ArrayDojo::ArrayDojo(DOJO::CArrayController array)
+ArrayDojo::ArrayDojo(DOJO::PageController array)
 {
-    value = CArrayControllerToVariant(array);
+    value = PageControllerToVariant(array);
 }
 
 ArrayDojo::ArrayDojo(DOJO::CArrayMemberValue array)
@@ -76,9 +76,9 @@ ArrayDojo::ArrayDojo(DOJO::CArrayCOptionFieldElement array)
     value = CArrayCOptionFieldElementToVariant(array);
 }
 
-ArrayDojo::ArrayDojo(DOJO::CArrayCHashItemFieldElementModelMetadata array)
+ArrayDojo::ArrayDojo(DOJO::CArrayModel array)
 {
-    value = CArrayCHashItemFieldElementModelMetadataToVariant(array);
+    value = CArrayModelToVariant(array);
 }
 
 // "Static" methods
@@ -224,9 +224,15 @@ Variant ArrayDojo::CArrayEnumOptionToVariant(DOJO::CArrayEnumOption array)
     return result;
 };
 
-Variant ArrayDojo::CArrayControllerToVariant(DOJO::CArrayController array)
+Variant ArrayDojo::PageControllerToVariant(const DOJO::PageController& page_controller)
 {
     TypedArray<Dictionary> result = {};
+    DOJO::CArrayController array = page_controller.items;
+    if (page_controller.next_cursor.tag == DOJO::COptionc_char_Tag::Somec_char)
+    {
+        Logger::debug_extra("PageController", "next_cursor", page_controller.next_cursor.some);
+
+    }
     std::vector<DOJO::Controller> array_controller_vector(array.data, array.data + array.data_len);
     for (const auto& controller : array_controller_vector)
     {
@@ -304,35 +310,31 @@ Variant ArrayDojo::CArrayCOptionFieldElementToVariant(DOJO::CArrayCOptionFieldEl
     return result;
 };
 
-Variant ArrayDojo::CArrayCHashItemFieldElementModelMetadataToVariant(
-    DOJO::CArrayCHashItemFieldElementModelMetadata array)
+Variant ArrayDojo::CArrayModelToVariant(
+    DOJO::CArrayModel array)
 {
-    std::vector<DOJO::CHashItemFieldElementModelMetadata> models_vec(
+    std::vector<DOJO::Model> models_vec(
     array.data, array.data + array.data_len
 );
     Array result = {};
-    for (const auto& model_item : models_vec)
+    for (const auto& model : models_vec)
     {
         Dictionary model_dict = {};
-        DOJO::ModelMetadata model_metadata = model_item.value;
 
-        Logger::debug_extra("METADATA", model_metadata.namespace_);
-        Logger::debug_extra("METADATA", model_metadata.name);
+        Logger::debug_extra("METADATA", model.namespace_);
+        Logger::debug_extra("METADATA", model.name);
 
-        model_dict["key"] = FieldElement(model_item.key).bytearray_deserialize(model_metadata.packed_size);
-        model_dict["namespace"] = model_metadata.namespace_;
-        model_dict["name"] = model_metadata.name;
-        model_dict["schema_type"] = String::num_int64(static_cast<int64_t>(model_metadata.schema.tag));
-
-        DojoTy metadata_ty = {model_metadata.schema};
+        DojoTy metadata_ty = {model.schema};
         model_dict["schema"] = metadata_ty.get_value();
 
-        model_dict["packed_size"] = Variant(model_metadata.packed_size);
-        model_dict["unpacked_size"] = Variant(model_metadata.unpacked_size);
-        model_dict["class_hash"] = FieldElement::get_as_string(&model_metadata.class_hash);
-        model_dict["contract_address"] = FieldElement::get_as_string(&model_metadata.contract_address);
-        ArrayDojo layout = {model_metadata.layout};
-        model_dict["layout"] = layout.get_value();
+        model_dict["namespace"] = model.namespace_;
+        model_dict["name"] = model.name;
+        model_dict["selector"] = FieldElement::get_as_string_no_ptr(model.selector);
+        model_dict["packed_size"] = Variant(model.packed_size);
+        model_dict["unpacked_size"] = Variant(model.unpacked_size);
+        model_dict["class_hash"] = FieldElement::get_as_string_no_ptr(model.class_hash);
+        model_dict["contract_address"] = FieldElement::get_as_string_no_ptr(model.contract_address);
+        model_dict["layout"] = model.layout;
         Logger::empty_line();
         result.append(model_dict);
     }
