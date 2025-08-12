ed604cbd2a0ab883c437c8786e03a08c6731e5b7
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-08-12 20:36:12 -0300

    refactor: Reorganize Dojo resource files

---
+++ include/resources/dojo_query_base.h
@@ -0,0 +1,70 @@
+//
+// Created by hazel on 7/8/25.
+//
+
+#ifndef DOJO_QUERY_BASE_H
+#define DOJO_QUERY_BASE_H
+#include "godot_cpp/core/class_db.hpp"
+
+#include "godot_cpp/classes/resource.hpp"
+
+#include "dojo_types.h"
+using namespace  godot;
+class DojoQueryBase : public Resource
+{
+    GDCLASS(DojoQueryBase, Resource);
+    
+    uint32_t limit;
+    String cursor;
+    DOJO::PaginationDirection direction;
+
+public:
+    DojoQueryBase()
+    {
+        limit = 10;
+        direction = DOJO::PaginationDirection::Forward;
+    };
+    ~DojoQueryBase(){};
+
+    virtual void* get_native_query() const { return nullptr; };
+
+    void set_limit(uint32_t p_limit) { limit = p_limit; }
+    uint32_t get_limit() const { return limit; }
+
+    void set_cursor(const String& p_cursor) { cursor = p_cursor; }
+    String get_cursor() const { return cursor; }
+
+    DOJO::PaginationDirection get_direction() const {return direction;}
+    void set_direction(DOJO::PaginationDirection p_direction) {direction = p_direction;}
+
+protected:
+    static void _bind_methods()
+    {
+        ClassDB::bind_method(D_METHOD("get_limit"), &DojoQueryBase::get_limit);
+        ClassDB::bind_method(D_METHOD("set_limit", "p_limit"), &DojoQueryBase::set_limit);
+        ADD_PROPERTY(PropertyInfo(Variant::INT, "limit"), "set_limit", "get_limit");
+
+        ClassDB::bind_method(D_METHOD("get_cursor"), &DojoQueryBase::get_cursor);
+        ClassDB::bind_method(D_METHOD("set_cursor", "p_cursor"), &DojoQueryBase::set_cursor);
+        ADD_PROPERTY(PropertyInfo(Variant::STRING, "cursor"), "set_cursor", "get_cursor");
+        
+        BIND_ENUM_CONSTANT(DOJO::PaginationDirection::Forward);
+        BIND_ENUM_CONSTANT(DOJO::PaginationDirection::Backward);
+
+        ClassDB::bind_method(D_METHOD("get_direction"), &DojoQueryBase::get_direction);
+        ClassDB::bind_method(D_METHOD("set_direction", "p_direction"), &DojoQueryBase::set_direction);
+        ADD_PROPERTY(PropertyInfo(
+                         Variant::INT,
+                         "direction",
+                         PROPERTY_HINT_ENUM,
+                         "Forward,Backward"
+                     ),
+                     "set_direction",
+                     "get_direction"
+        );
+
+    };
+};
+VARIANT_ENUM_CAST(DOJO::PaginationDirection);
+
+#endif //DOJO_QUERY_BASE_H
---
+++ include/resources/dojo_subscription.h
@@ -0,0 +1,96 @@
+#ifndef DOJO_SUBSCRIPTION_H
+#define DOJO_SUBSCRIPTION_H
+
+#include "dojo_types.h"
+#include "godot_cpp/classes/resource.hpp"
+#include "tools/logger.h"
+
+using namespace godot;
+
+class DojoSubscription : public Resource {
+    GDCLASS(DojoSubscription, Resource);
+
+    Callable callback;
+    DOJO::Subscription* subscription = nullptr;
+    String name;
+
+public:
+    enum Type {
+        EVENT,
+        MESSAGE,
+        ENTITY,
+        TOKEN,
+        TOKEN_BALANCE,
+        TRANSACTION,
+        STARKNET,
+        INDEXER,
+        NIL
+    };
+
+    virtual ~DojoSubscription() {
+        cancel();
+    }
+
+    void cancel() {
+        if (subscription) {
+            Logger::info("Subscription '", name, "' canceled");
+            DOJO::subscription_cancel(subscription);
+            subscription = nullptr;
+        }
+    }
+
+    Callable get_callback() const {
+        return callback;
+    }
+
+    void set_callback(const Callable& p_callback) {
+        callback = p_callback;
+    }
+
+    String get_name() const { return name; }
+    void set_name(const String& p_name) { name = p_name; }
+
+    DOJO::Subscription* get_subscription() const { return subscription; }
+    void set_subscription(DOJO::Subscription* p_subscription) { this->subscription = p_subscription; }
+
+    virtual Type get_type() const { return NIL; }
+
+protected:
+    static void _bind_methods() {
+        ClassDB::bind_method(D_METHOD("set_callback", "p_callback"), &DojoSubscription::set_callback);
+        ClassDB::bind_method(D_METHOD("get_callback"), &DojoSubscription::get_callback);
+        ADD_PROPERTY(PropertyInfo(Variant::CALLABLE, "callback"), "set_callback", "get_callback");
+
+        ClassDB::bind_method(D_METHOD("set_name", "p_name"), &DojoSubscription::set_name);
+        ClassDB::bind_method(D_METHOD("get_name"), &DojoSubscription::get_name);
+        ADD_PROPERTY(PropertyInfo(Variant::STRING, "name"), "set_name", "get_name");
+
+        ClassDB::bind_method(D_METHOD("cancel"), &DojoSubscription::cancel);
+        ClassDB::bind_method(D_METHOD("get_type"), &DojoSubscription::get_type);
+
+        ADD_PROPERTY(PropertyInfo(
+                         Variant::INT,
+                         "type",
+                         PROPERTY_HINT_ENUM,
+                         "Event,Message,Entity,Token,TokenBalance,Transaction,Starknet,Indexer,Nil",
+                         PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_READ_ONLY
+                     ),
+                     "", // No setter
+                     "get_type"
+        );
+
+        BIND_ENUM_CONSTANT(EVENT);
+        BIND_ENUM_CONSTANT(MESSAGE);
+        BIND_ENUM_CONSTANT(ENTITY);
+        BIND_ENUM_CONSTANT(TOKEN);
+        BIND_ENUM_CONSTANT(TOKEN_BALANCE);
+        BIND_ENUM_CONSTANT(TRANSACTION);
+        BIND_ENUM_CONSTANT(STARKNET);
+        BIND_ENUM_CONSTANT(INDEXER);
+        BIND_ENUM_CONSTANT(NIL);
+    }
+};
+
+VARIANT_ENUM_CAST(DojoSubscription::Type);
+
+#endif //DOJO_SUBSCRIPTION_H
