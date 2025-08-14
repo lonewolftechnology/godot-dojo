1e4c85c3e00e985b7f3c21b08c9260b8909bcc07
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-08-14 17:49:37 -0300

    update demo

--- demo/game/demo.gd
+++ demo/game/demo.gd
@@ -12,10 +12,10 @@ extends Node
 
 
 enum Directions {
-	LEFT = 0,
-	RIGHT = 1,
-	UP = 2,
-	DOWN = 3,
+    LEFT = 0,
+    RIGHT = 1,
+    UP = 2,
+    DOWN = 3,
 }
 
 @export var spawn_call : DojoCall
@@ -23,6 +23,7 @@ enum Directions {
 
 @export var connection : DojoConnection 
 
+@export var controller_query:DojoControllerQuery
 
 @onready var label_username: Label = %LabelUsername
 @onready var label_address: Label = %LabelAddress
@@ -33,189 +34,181 @@ enum Directions {
 var _player_address : String = ""
 
 func _ready() -> void:
-	OS.set_environment("RUST_BACKTRACE", "full")
-	OS.set_environment("RUST_LOG", "debug")
-	connection.controller_account.current_user_info.connect(_on_controller_current_user_info)
+    OS.set_environment("RUST_BACKTRACE", "full")
+    OS.set_environment("RUST_LOG", "debug")
+    connection.controller_account.current_user_info.connect(_on_controller_current_user_info)
 
 func _on_controller_current_user_info(data:Dictionary) -> void:
-	var address : String = data['address']
-	var username : String = data['username']
-	_player_address = address
-	label_username.text = "Username: %s"%username
-	var player_entity := controllers_manager.spawn_entity(address,true)
-	player_entity.set_username(username)
+    var address : String = data['address']
+    var username : String = data['username']
+    _player_address = address
+    label_username.text = "Username: %s"%username
+    var player_entity := controllers_manager.spawn_entity(address,true)
+    player_entity.set_username(username)
 
 
 func _on_events(args:Dictionary) -> void:
-	_handle_callback(args)
+    push_warning("CALLBACK EVENTS", args)
+    _handle_callback(args)
 
 func _on_entities(args:Dictionary) -> void:
-	_handle_callback(args)
+    push_warning("CALLBACK ENTITIES", args)
+    _handle_callback(args)
 
 func _handle_callback(args:Dictionary) -> void:
-	var data = args["data"]
-	# This is contract specific and data structure may change
-	var result_data : Dictionary
-	for entry : Dictionary in data:
-		result_data.merge(entry)
+    print(args)
+    return
+    var data = args["data"]
+    # This is contract specific and data structure may change
+    var result_data : Dictionary
+    for entry : Dictionary in data:
+        result_data.merge(entry)
 
-	await get_tree().process_frame
-	if result_data.has("Vec2"):
-		var pos := Vector2(result_data['Vec2']['x'], result_data['Vec2']['y'] )
-		var id : String = result_data['player']
-		controllers_manager.move_controller(id, pos)
-	if result_data.has("remaining"):
-		var id : String = result_data['player']
-		if id == _player_address:
-			label_moves.text = "Moves: %s" % result_data['remaining']
+    await get_tree().process_frame
+    if result_data.has("Vec2"):
+        var pos := Vector2(result_data['Vec2']['x'], result_data['Vec2']['y'] )
+        var id : String = result_data['player']
+        controllers_manager.move_controller(id, pos)
+    if result_data.has("remaining"):
+        var id : String = result_data['player']
+        if id == _player_address:
+            label_moves.text = "Moves: %s" % result_data['remaining']
 
 
 func spawn() -> void:
-	connection.controller_account.execute_from_outside(spawn_call)
+    connection.controller_account.execute_from_outside(spawn_call)
 
 
 func _on_start_screen_entered() -> void:
-	start()
+    start()
 
 func start() -> void:
-	await get_tree().create_timer(0.1).timeout
-	connection.create_subscriptions(_on_events,_on_entities)
-	
-	await get_tree().create_timer(0.1).timeout
-	spawn()
-	
-	await get_tree().create_timer(0.1).timeout
-	
-	var parsed : Dictionary = get_entities()
-	
-	for id in parsed.keys():
-		controllers_manager.spawn_entity(id)
-	
-	
-	#await get_tree().create_timer(0.1).timeout
-	_update_entities(parsed)
-	
-	
-	get_controllers(parsed.keys())
+    await get_tree().create_timer(0.1).timeout
+    connection.create_subscriptions(_on_events,_on_entities)
+    
+    await get_tree().create_timer(0.1).timeout
+    spawn()
+    
+    await get_tree().create_timer(0.1).timeout
+    
+    var parsed : Dictionary = get_entities()
+    
+    for id in parsed.keys():
+        controllers_manager.spawn_entity(id)
+    
+    
+    #await get_tree().create_timer(0.1).timeout
+    _update_entities(parsed)
+    
+    
+    get_controllers(parsed.keys())
 
 
 func get_controllers(addrs:Array = []) -> void:
-	print("&&&&&&& Addresses: %s\n"%str(addrs))
-	var data = connection.client.get_controllers([],addrs)
-	print("######\nControllers: %s\n######\n"%str(data))
-	for controller in data:
-		var id : String = controller["address"]
-		var username : String = controller["username"]
-		var entity : GenericEntity = controllers_manager.get_entity(id)
-		if entity == null:
-			entity = controllers_manager.spawn_entity(id)
-		
-		entity.set_username(username)
+    print("&&&&&&& Addresses: %s\n"%str(addrs))
+    var data = connection.client.get_controllers(controller_query)
+    print("######\nControllers: %s\n######\n"%str(data))
+    for controller in data:
+        var id : String = controller["address"]
+        var username : String = controller["username"]
+        var entity : GenericEntity = controllers_manager.get_entity(id)
+        if entity == null:
+            entity = controllers_manager.spawn_entity(id)
+        
+        entity.set_username(username)
 
 
 func get_entities() -> Dictionary:
-	var query = {
-		"pagination":{
-			"limit": 10,
-			"cursor": "",
-			"order_by": [],
-			"direction": ToriiClient.QueryPaginationDirection.FORWARD
-		},
-		"clause": null,
-		"no_hashed_keys": true,
-		"models":[],
-		"historical": false
-	}
-	var data = connection.client.get_entities(query)
-	var parsed_entities : Dictionary = {}
-	for entity in data:
-		for model:Dictionary in entity.models:
-			var id : String = ""
-			var position : Vector2 = Vector2.ZERO
-			var can_move : bool = true
-			var remaining : int = 100
-			for key in model:
-				var entry:Dictionary = model[key]
-				#print("###### KEY: %s\n###### ENTRY: %s\n"%[str(key),str(entry)])
-				id = entry["player"]
-				match key:
-					"dojo_starter-Position":#,"dojo_starter-PositionSigned","dojo_starter-PositionI32":
-						var v : Dictionary = {}
-						
-						if entry.has("vec"):
-							v = entry["vec"]
-							
-						elif entry.has("Vector2Signed"):
-							v = entry["Vector2Signed"]
-						var x := v["x"] as float
-						var y := v["y"] as float
-						position = Vector2(x,y)
-						
-						
-						if not parsed_entities.has(id):
-							parsed_entities[id] = {}
-						
-						parsed_entities[id] = {}
-						parsed_entities[id].merge({"position": position})
-					"dojo_starter-Moves":
-						remaining = entry["remaining"] as int
-						can_move = entry["can_move"] as bool
-						
-						if not parsed_entities.has(id):
-							parsed_entities[id] = {}
-							
-						parsed_entities[id]["remaining"] = remaining
-						parsed_entities[id]["can_move"] = can_move
-					"dojo_starter-U128Value":
-						pass
-					"dojo_starter-U256Value":
-						pass
-	return parsed_entities
+    var data = connection.client.get_entities(DojoQuery.new())
+    var parsed_entities : Dictionary = {}
+    for entity in data:
+        for model:Dictionary in entity.models:
+            var id : String = ""
+            var position : Vector2 = Vector2.ZERO
+            var can_move : bool = true
+            var remaining : int = 100
+            for key in model:
+                var entry:Dictionary = model[key]
+                #print("###### KEY: %s\n###### ENTRY: %s\n"%[str(key),str(entry)])
+                id = entry["player"]
+                match key:
+                    "dojo_starter-Position":#,"dojo_starter-PositionSigned","dojo_starter-PositionI32":
+                        var v : Dictionary = {}
+                        
+                        if entry.has("vec"):
+                            v = entry["vec"]
+                            
+                        elif entry.has("Vector2Signed"):
+                            v = entry["Vector2Signed"]
+                        var x := v["x"] as float
+                        var y := v["y"] as float
+                        position = Vector2(x,y)
+                        
+                        
+                        if not parsed_entities.has(id):
+                            parsed_entities[id] = {}
+                        
+                        parsed_entities[id] = {}
+                        parsed_entities[id].merge({"position": position})
+                    "dojo_starter-Moves":
+                        remaining = entry["remaining"] as int
+                        can_move = entry["can_move"] as bool
+                        
+                        if not parsed_entities.has(id):
+                            parsed_entities[id] = {}
+                            
+                        parsed_entities[id]["remaining"] = remaining
+                        parsed_entities[id]["can_move"] = can_move
+                    "dojo_starter-U128Value":
+                        pass
+                    "dojo_starter-U256Value":
+                        pass
+    return parsed_entities
 
 
 func _update_entities(parsed_entities:Dictionary) -> void:
-	for key in parsed_entities.keys():
-		var id : String = key
-		if parsed_entities[key].has("position"):
-			var position : Vector2 = parsed_entities[key]["position"]
-			controllers_manager.move_controller(id,position)
+    for key in parsed_entities.keys():
+        var id : String = key
+        if parsed_entities[key].has("position"):
+            var position : Vector2 = parsed_entities[key]["position"]
+            controllers_manager.move_controller(id,position)
 
 
 
 func _move(dir:Directions) -> void:
-	move_call.calldata[0] = dir
-	var u32 = DojoHelpers.signed_to_u32_offset(%StepsAmount.value)
-	move_call.calldata[1] = u32
-	push_warning(u32)
-	connection.controller_account.execute_from_outside(move_call)
+    move_call.calldata[0] = dir
+    move_call.calldata[1] = int(%StepsAmount.value)
+    connection.controller_account.execute_from_outside(move_call)
 
 func _on_arrow_left_pressed() -> void:
-	_move(Directions.LEFT)
+    _move(Directions.LEFT)
 
 func _on_arrow_up_pressed() -> void:
-	_move(Directions.UP)
+    _move(Directions.UP)
 
 func _on_arrow_down_pressed() -> void:
-	_move(Directions.DOWN)
+    _move(Directions.DOWN)
 
 func _on_arrow_right_pressed() -> void:
-	_move(Directions.RIGHT)
+    _move(Directions.RIGHT)
 
 
 #func _on_move_to_pressed() -> void:
-	#var x:int = %Vx.value
-	#var y:int = %Vy.value
-	#move_to.calldata[0] = Vector2i(x,y)
-	#connection.controller_account.execute_from_outside(move_to)
+    #var x:int = %Vx.value
+    #var y:int = %Vy.value
+    #move_to.calldata[0] = Vector2i(x,y)
+    #connection.controller_account.execute_from_outside(move_to)
 
 
 func _on_disconnect_pressed() -> void:
-	connection.controller_account.disconnect_controller()
-	controllers_manager.clear()
-	await get_tree().process_frame	
-	get_tree().reload_current_scene()
+    connection.controller_account.disconnect_controller()
+    controllers_manager.clear()
+    await get_tree().process_frame	
+    get_tree().reload_current_scene()
 
 
 func _on_move_to_pressed() -> void:
-	var data = connection.client.get_controllers(["hazel", "danntheoperator", "dtodice"])
-	print("######\nControllers: %s\n######\n"%str(data))
+    var testing:DojoControllerQuery = DojoControllerQuery.new()
+    testing.usernames = ["hazel", "danntheoperator", "dtodice"]
+    var data = connection.client.get_controllers(testing)
+    print("######\nControllers: %s\n######\n"%str(data))
