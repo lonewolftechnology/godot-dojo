30cc77bfbe425231ec0ef54fcfb5325074790c09
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-08-25 11:35:36 -0300

    add use slot or use local katana/torii for unittesting

--- demo/debug/unit_testing/account.gd
+++ demo/debug/unit_testing/account.gd
@@ -1,7 +1,11 @@
 extends Node
 
+const local_rpc_url = "http://localhost:5050"
+const local_torii_url = "http://localhost:8080"
+
 const rpc_url = "https://api.cartridge.gg/x/godot-demo-rookie/katana"
 const torii_url = "https://api.cartridge.gg/x/godot-demo-rookie/torii"
+
 const contract_address = "0x038c3535090be3807d95354db3ae8dc0ceb19a0240739a3338cbc0d6c8ee47b4"
 const world_address = "0x073c2b317136214562b608523812f369a05efe67257842a17c4985ce6d390be7"
 
@@ -37,11 +41,18 @@ const tests = {
 @export var entity_sub:EntitySubscription
 @export var event_message_sub:MessageSubscription
 
+@export var use_slot:bool = true
+
 func _ready() -> void:
 	OS.set_environment("RUST_BACKTRACE", "full")
 	OS.set_environment("RUST_LOG", "debug")
-	torii_client.torii_url = torii_url
 	torii_client.world_address = world_address
+	
+	if use_slot:
+		torii_client.torii_url = torii_url
+	else:
+		torii_client.torii_url = local_torii_url
+
 	torii_client.create_client()
 
 func _on_events(args:Dictionary) -> void:
@@ -60,15 +71,20 @@ func _on_torii_client_client_connected(success: bool) -> void:
 	#torii_client.on_event_message_update(_on_events_message, event_message_sub)
 	
 	await get_tree().create_timer(0.1).timeout
-	account.create(rpc_url, account_address, private_key)
+	if use_slot:
+		account.create(rpc_url, account_address, private_key)
+	else:
+		account.create(local_rpc_url, account_address, private_key)
+		
 	await get_tree().create_timer(0.1).timeout
-	account.set_block_id("0x0")
+	account.set_block_id()
 	await get_tree().create_timer(0.1).timeout
-	for selector in tests.keys():
-		var data = tests[selector]
-		for calldata in data:
-			account.execute_raw(contract_address, selector, [calldata])
-			await get_tree().process_frame
+	if account.is_account_valid():
+		for selector in tests.keys():
+			var data = tests[selector]
+			for calldata in data:
+				account.execute_raw(contract_address, selector, [calldata])
+				await get_tree().process_frame
 
 
 func _on_account_transaction_failed(error_message: Dictionary) -> void:
