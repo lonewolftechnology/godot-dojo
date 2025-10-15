1182d74f5ddf3cbe8abd873bb69e59d9e2796827
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-10-15 18:34:24 -0300

    ProjectSettings: update initial config

--- src/dojo_c_gdextension.cpp
+++ src/dojo_c_gdextension.cpp
@@ -37,22 +37,22 @@ void DojoC::init_config(bool reset)
     {
         set_setting("dojo/config/disconnect_controller_on_exit", false, reset);
         set_setting("dojo/config/katana_url", "http://localhost:5050", reset);
-        set_setting("dojo/config/torii_url", "http://localhost:8080", reset);
-
-        set_setting("dojo/config/contract_address", "0x0", reset);
-        set_setting("dojo/config/world_address", "0x0", reset);
-
+        // Torii Config
+        set_setting("dojo/config/torii/torii_url", "http://localhost:8080", reset);
+        set_setting("dojo/config/torii/contract_addresses", TypedArray<String>(), reset);
+        set_setting("dojo/config/torii/worlds", TypedArray<String>(), reset);
+        // Policies
         Dictionary policies_info = {};
         policies_info["name"] = "dojo/config/policies";
         policies_info["type"] = Variant::DICTIONARY;
-
+        // Account
         set_setting("dojo/config/account/address", "0x0",
                     reset);
         set_setting("dojo/config/account/public_key",
                     "0x0", reset);
         set_setting("dojo/config/account/private_key",
                     "0x0", reset);
-
+        // Policies
         set_setting("dojo/config/policies", Dictionary(), reset);
         // Format: [VariantType]/[Hint]:[ClassName]
 
@@ -64,11 +64,10 @@ void DojoC::init_config(bool reset)
 
         }
 
-
         ProjectSettings::get_singleton()->add_property_info(policies_info);
 
         // set_setting("dojo/config/chain_id", "KATANA", reset);
-
+        // Fixed Point
         set_setting("dojo/config/fixed_point/default", 128, reset);
         set_setting("dojo/config/fixed_point/64", 40, reset);
         set_setting("dojo/config/fixed_point/128", 60, reset);
