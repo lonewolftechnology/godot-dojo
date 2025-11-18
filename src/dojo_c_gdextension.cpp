14ae10913d707c66bd5c8964f2609e0353e7d489
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-11-18 17:13:05 -0300

    ProjectSettings: remove initial configuration for policies

--- src/dojo_c_gdextension.cpp
+++ src/dojo_c_gdextension.cpp
@@ -36,17 +36,14 @@ void DojoC::init_config(bool reset)
     if (Engine::get_singleton()->is_editor_hint())
     {
         set_setting("dojo/config/enable_extra_logs", false, reset);
-        set_setting("dojo/config/disconnect_controller_on_exit", false, reset);
+
         set_setting("dojo/config/katana_url", "http://localhost:5050", reset);
         set_setting("dojo/config/contract_address", "0x0", reset);
         // Torii Config
+        // TODO: multiple contracts
         set_setting("dojo/config/torii/torii_url", "http://localhost:8080", reset);
         // set_setting("dojo/config/torii/contract_addresses", TypedArray<String>(), reset);
         set_setting("dojo/config/torii/worlds", TypedArray<String>(), reset);
-        // Policies
-        Dictionary policies_info = {};
-        policies_info["name"] = "dojo/config/policies";
-        policies_info["type"] = Variant::DICTIONARY;
         // Account
         set_setting("dojo/config/account/address", "0x0",
                     reset);
@@ -54,21 +51,7 @@ void DojoC::init_config(bool reset)
                     "0x0", reset);
         set_setting("dojo/config/account/private_key",
                     "0x0", reset);
-        // Policies
-        set_setting("dojo/config/policies", Dictionary(), reset);
-        // Format: [VariantType]/[Hint]:[ClassName]
 
-        if (DojoHelpers::can_use_typed_dictionaries())
-        {
-            // PROPERTY_HINT_DICTIONARY_TYPE = 38 in Godot 4.4+
-            policies_info["hint"] = vformat("%d/%d", Variant::DICTIONARY, 38);
-            policies_info["hint_string"] = vformat("%s;%s", "String", "String");
-
-        }
-
-        ProjectSettings::get_singleton()->add_property_info(policies_info);
-
-        // set_setting("dojo/config/chain_id", "KATANA", reset);
         // Fixed Point
         set_setting("dojo/config/fixed_point/default", 128, reset);
         set_setting("dojo/config/fixed_point/64", 40, reset);
