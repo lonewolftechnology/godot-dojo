e3574b228939f109a58c2d416880ee5b7d8db9ff
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-11-19 16:49:20 -0300

    StarterProject: fixed http serving

--- dojo-starter-godot-project/connection/http_tools.gd
+++ dojo-starter-godot-project/connection/http_tools.gd
@@ -1,7 +1,6 @@
 class_name HttpTools
 extends Node
 
-var host:String = "http://localhost"
 var port:int = 27000
 var server:TCPServer
 var clients:Array[StreamPeerTCP]
@@ -10,34 +9,35 @@ var redirect_query_name:String = "startapp"
 @export_file_path("*.html") var html_file:String
 
 func _ready() -> void:
-	set_process(false)
 	start_server()
 	
 func _process(delta: float) -> void:
-	if clients.is_empty():
-		set_process(false)
 	if server.is_connection_available():
 		var client:StreamPeerTCP = server.take_connection()
 		
-		if client.is_valid():
+		if client:
 			clients.push_back(client)
 	
 	for client:StreamPeerTCP in clients:
-		if client.is_valid():
-			if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
-				var available_bytes = client.get_available_bytes()
-				if available_bytes > 0:
-					var client_data:Array = client.get_data(available_bytes)
-					var data:PackedByteArray = client_data[1]
-					var request_string = data.get_string_from_utf8()
-					var payload:String = _get_padded_base64(filter_response(redirect_query_name + "=", request_string))
-					var parsed_paylod:String = Marshalls.base64_to_utf8(payload)
-					
-					var response:Dictionary = JSON.parse_string(parsed_paylod)
-					send_http_response(client, response)
+		if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+			var available_bytes = client.get_available_bytes()
+			if available_bytes > 0:
+				var client_data:Array = client.get_data(available_bytes)
+				var data:PackedByteArray = client_data[1]
+				var request_string = data.get_string_from_utf8()
+				var payload:String = _get_padded_base64(filter_response(redirect_query_name + "=", request_string))
+				
+				if payload.is_empty():
 					client.disconnect_from_host()
-					
-					DisplayServer.window_request_attention()
+					continue
+				
+				var parsed_payload:String = Marshalls.base64_to_utf8(payload)
+				
+				var response:Dictionary = JSON.parse_string(parsed_payload)
+				send_http_response(client, response)
+				client.disconnect_from_host()
+				
+				DisplayServer.window_request_attention()
 		else:
 			clients.erase(client)
 				
@@ -79,9 +79,8 @@ func send_http_response(client: StreamPeerTCP, response:Dictionary):
 		"transaction_hash" : transaction_hash,
 		"expires_at" : expires_at
 	}
-	var _file = FileAccess.open(html_file, FileAccess.READ)
-	var html_template:String = _file.get_as_text()
-	
+
+	var html_template:String = get_template()
 	var html_content:String = html_template.format(format_args)
 	
 	var http_response:String = "HTTP/1.1 200 OK\r\n";
@@ -93,20 +92,28 @@ func send_http_response(client: StreamPeerTCP, response:Dictionary):
 	
 	client.put_data(http_response.to_utf8_buffer())
 
-func setup(_host:String, _port:int) -> void:
-	host = _host
+func setup(_port:int) -> void:
 	port = _port
 	start_server()
 	
 func start_server() -> bool:
+	if server != null and server.is_listening():
+		return true
+
 	server = TCPServer.new()
 	
-	var err:int = server.listen(port,host)
+	var err:int = server.listen(port)
 	if not err == OK:
 		push_error("Error initializing http server")
 		set_process(false)
 		return false
 	
 	set_process(true)
-	print_rich("[color=green] Server listening on %s:%d" % [host, port])
+	print_rich("[color=green] Server listening on http://localhost:%d" %  port)
 	return true
+
+func get_template() -> String:
+	var _file: FileAccess = FileAccess.open(html_file, FileAccess.READ)
+	if not _file.get_error() == OK:
+		push_error("failed opening html template")
+	return _file.get_as_text()
