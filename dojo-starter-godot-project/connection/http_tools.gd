222c6b78957396cf2923b20ec5073345c8bd33d4
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-11-21 17:21:52 -0300

    StarterProject: Stylize template callback

--- dojo-starter-godot-project/connection/http_tools.gd
+++ dojo-starter-godot-project/connection/http_tools.gd
@@ -24,29 +24,33 @@ func _process(delta: float) -> void:
 	
 	for client:StreamPeerTCP in clients:
 		if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
-			var available_bytes = client.get_available_bytes()
-			if available_bytes > 0:
-				var client_data:Array = client.get_data(available_bytes)
-				var data:PackedByteArray = client_data[1]
-				var request_string = data.get_string_from_utf8()
-				var payload:String = _get_padded_base64(filter_response(redirect_query_name + "=", request_string))
+			send_http_response(client)
+			client.disconnect_from_host()
+			
+			DisplayServer.window_request_attention()		
+#			var available_bytes = client.get_available_bytes()
+#			if available_bytes > 0:
+#				var client_data:Array = client.get_data(available_bytes)
+#				var data:PackedByteArray = client_data[1]
+#				var request_string = data.get_string_from_utf8()
+#				var payload:String = _get_padded_base64(filter_response(redirect_query_name + "=", request_string))
 				
-				if payload.is_empty():
-					client.disconnect_from_host()
-					continue
+#				if payload.is_empty():
+#					client.disconnect_from_host()
+#					continue
 				
-				var parsed_payload:String = Marshalls.base64_to_utf8(payload)
+#				var parsed_payload:String = Marshalls.base64_to_utf8(payload)
 				
-				var response:Variant = JSON.parse_string(parsed_payload)
-				if response == null:
-					push_error("HTTP Tools: Failed to parse JSON payload or payload was empty.")
-					client.disconnect_from_host()
-					continue
+#				var response:Variant = JSON.parse_string(parsed_payload)
+#				if response == null:
+#					push_error("HTTP Tools: Failed to parse JSON payload or payload was empty.")
+#					client.disconnect_from_host()
+#					continue
 
-				send_http_response(client, response)
-				client.disconnect_from_host()
-				
-				DisplayServer.window_request_attention()
+#				send_http_response(client)
+#				client.disconnect_from_host()
+#				
+#				DisplayServer.window_request_attention()
 		else:
 			clients.erase(client)
 				
@@ -73,32 +77,32 @@ func filter_response(marker:String, response:String) -> String:
 	
 	return result
 	
-func send_http_response(client: StreamPeerTCP, response:Dictionary) -> void:
-	var username:String = response["username"]
-	var address:String = response["address"]
-	var owner_guid:String = response["ownerGuid"]
-	var transaction_hash:String = response["transactionHash"]
-	var time_dict:Dictionary = Time.get_datetime_dict_from_unix_time(int(response["expiresAt"]))
-	var expires_at:String = Time.get_datetime_string_from_datetime_dict(time_dict, true)
-	
-
-	var format_args:Dictionary = {
-		"username" : username,
-		"address" : address,
-		"owner_guid" : owner_guid,
-		"transaction_hash" : transaction_hash,
-		"expires_at" : expires_at
-	}
-
+func send_http_response(client: StreamPeerTCP) -> void:
+#	var username:String = response["username"]
+#	var address:String = response["address"]
+#	var owner_guid:String = response["ownerGuid"]
+#	var transaction_hash:String = response["transactionHash"]
+#	var time_dict:Dictionary = Time.get_datetime_dict_from_unix_time(int(response["expiresAt"]))
+#	var expires_at:String = Time.get_datetime_string_from_datetime_dict(time_dict, true)
+#	
+#
+#	var format_args:Dictionary = {
+#		"username" : username,
+#		"address" : address,
+#		"owner_guid" : owner_guid,
+#		"transaction_hash" : transaction_hash,
+#		"expires_at" : expires_at
+#	}
+#
 	var html_template:String = get_template()
-	var html_content:String = html_template.format(format_args)
+#	var html_content:String = html_template.format(format_args)
 	
 	var http_response:String = "HTTP/1.1 200 OK\r\n";
 	http_response += "Content-Type: text/html; charset=UTF-8\r\n";
-	http_response += "Content-Length: " + str(html_content.to_utf8_buffer().size()) + "\r\n";
+	http_response += "Content-Length: " + str(html_template.to_utf8_buffer().size()) + "\r\n";
 	http_response += "Connection: close\r\n";
 	http_response += "\r\n";
-	http_response += html_content;
+	http_response += html_template;
 	
 	client.put_data(http_response.to_utf8_buffer())
 	
