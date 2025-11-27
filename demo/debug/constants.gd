2393b78510e678ad24b90d463862c632876e7b90
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-11-27 10:45:36 -0300

    demo: moved dojo config to its own static class

---
+++ demo/debug/constants.gd
@@ -0,0 +1,7 @@
+class_name Constants
+
+const WORLD: String = "0x06560acafbd6c554e7458234c3bcf5636a18c72a6afe3099162fb8bdb12f4ef7"
+const CONTRACT: String = "0x016de9d5d6d7f39930330d9cccb224d741794b3723cb7d50ddf23c4837fe3360"
+
+const RPC_URL: String = "https://api.cartridge.gg/x/godot-dojo-starter/katana"
+const TORII_URL: String = "https://api.cartridge.gg/x/godot-dojo-starter/torii"
