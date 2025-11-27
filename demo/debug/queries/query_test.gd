9f4e92101890ff15322cd0157900c8a395bf6a31
Daniel Rodriguez Aires <dannroda@gmail.com>
2025-11-27 10:48:39 -0300

    demo: wip new modular `QueryTest`

---
+++ demo/debug/queries/query_test.gd
@@ -0,0 +1,56 @@
+extends Control
+@export_category("ToriiTest")
+@onready var address_input:LineEdit = %AddressInput
+@onready var model_input:LineEdit = %ModelInput
+@onready var member_input:LineEdit = %MemberInput
+@onready var output_box:RichTextLabel = %Output
+
+@export var torii_client:ToriiClient
+
+func _ready() -> void:
+	if get_parent() == get_tree().root:
+		torii_client = ToriiClient.new()
+		add_child(torii_client)
+		torii_client.create_client(Constants.TORII_URL)
+
+func _on_query_pressed() -> void:
+	print("aaaaaaa")
+	var query:DojoQuery = DojoQuery.new()
+	var clause = DojoOptionClause.new()
+	clause.tag = 2
+	clause.model = model_input.text
+	clause.member = member_input.text
+	clause.primitive_tag = 14
+	clause.value = address_input.text
+	query.clause = clause
+	
+	_send_query(query)
+	
+
+func _on_clear_pressed() -> void:
+	output_box.text = ""
+
+
+func _on_empty_query_pressed() -> void:
+	var query:DojoQuery = DojoQuery.new()
+	_send_query(query)
+
+
+func _send_query(query:DojoQuery):
+	var data:Array = torii_client.get_entities(query)
+	if data.is_empty(): return
+	print(data)
+	data.any(_parse_query)
+
+	output_box.append_text("------------------------\n")
+
+func _parse_query(response:Dictionary):
+	var models:Array = response["models"]
+	models.any(_parse_model)
+	output_box.append_text("\n")
+
+func _parse_model(model:Dictionary):
+	var _name = model.keys()[0]
+	var model_data:Dictionary = model[_name]
+	output_box.append_text("%s %s\n" % [_name, model_data["player"]])
+	print_rich("[color=green]FOUND[/color] ", _name, " [color=cyan]ADDR[/color] ", model_data["player"])
