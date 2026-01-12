4a5152a88220bfeb41e8d3bf91417e42812028fb
Daniel Rodriguez Aires <dannroda@gmail.com>
2026-01-12 15:42:32 -0300

    wip: entityQuery for testing dojo.hpp

--- include/ref_counted/queries/entity.hpp
+++ include/ref_counted/queries/entity.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ref_counted/query_builder.hpp"
-#include "../dojo_utilities/clause.hpp"
+#include "ref_counted/dojo_utilities/clause.hpp"
 
 class EntityQuery : public QueryBuilder {
     GDCLASS(EntityQuery, QueryBuilder)
@@ -21,9 +21,15 @@ public:
     Ref<EntityQuery> hashed_keys(const bool& hashed_keys);
     Ref<EntityQuery> models(const PackedStringArray& models);
     Ref<EntityQuery> historical(const bool& historical);
+    Ref<EntityQuery> with_clause(const Ref<DojoClause>& clause);
 
 protected:
     void static _bind_methods() {
         ClassDB::bind_method(D_METHOD("world_addresses", "worlds"), &EntityQuery::world_addresses);
+        ClassDB::bind_method(D_METHOD("add_world", "world"), &EntityQuery::add_world);
+        ClassDB::bind_method(D_METHOD("hashed_keys", "hashed_keys"), &EntityQuery::hashed_keys);
+        ClassDB::bind_method(D_METHOD("models", "models"), &EntityQuery::models);
+        ClassDB::bind_method(D_METHOD("historical", "historical"), &EntityQuery::historical);
+        ClassDB::bind_method(D_METHOD("with_clause", "clause"), &EntityQuery::with_clause);
     }
 };
