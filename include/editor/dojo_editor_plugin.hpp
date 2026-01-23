1db61f6aa06d47232414584a9d63cb0d4dc7d09b
Daniel Rodriguez Aires <dannroda@gmail.com>
2026-01-23 14:06:36 -0300

    feat: reorganized tools menu and added check for new releases

--- include/editor/dojo_editor_plugin.hpp
+++ include/editor/dojo_editor_plugin.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "godot_cpp/classes/editor_plugin.hpp"
+#include "godot_cpp/classes/http_request.hpp"
 #include "tools/logger.hpp"
 
 using namespace godot;
@@ -32,6 +33,11 @@ public:
     void init_config(bool reset = false);
     void set_setting(const String& setting, const Variant& value, const bool& force = false, bool p_save = true);
 
+    void _on_menu_item_pressed(int id);
+
+    void check_for_updates();
+    void _on_release_request_completed(int result, int response_code, const PackedStringArray& headers, const PackedByteArray& body, HTTPRequest* http);
+
 protected:
     static void _bind_methods()
     {
@@ -42,6 +48,7 @@ protected:
         ClassDB::bind_method(D_METHOD("reset_project_settings"), &DojoEditorPlugin::reset_project_settings);
         ClassDB::bind_method(D_METHOD("print_version"), &DojoEditorPlugin::print_version);
         ClassDB::bind_method(D_METHOD("_get_plugin_version"), &DojoEditorPlugin::_get_plugin_version);
+        ClassDB::bind_method(D_METHOD("check_for_updates"), &DojoEditorPlugin::check_for_updates);
         ClassDB::bind_method(D_METHOD("init_config", "reset"), &DojoEditorPlugin::init_config);
         ClassDB::bind_method(D_METHOD("init_setting", "setting", "value", "force", "save"),
                                     &DojoEditorPlugin::set_setting, DEFVAL(true), DEFVAL(false));
--- src/editor/dojo_editor_plugin.cpp
+++ src/editor/dojo_editor_plugin.cpp
@@ -3,8 +3,14 @@
 //
 
 #include "editor/dojo_editor_plugin.hpp"
+
+#include <godot_cpp/classes/editor_interface.hpp>
+
 #include "godot_cpp/classes/project_settings.hpp"
 #include "godot_cpp/classes/engine.hpp"
+#include "godot_cpp/classes/json.hpp"
+#include "godot_cpp/classes/popup_menu.hpp"
+#include "godot_cpp/classes/reg_ex.hpp"
 #include "tools/logger.hpp"
 
 DojoEditorPlugin::DojoEditorPlugin()
@@ -24,16 +30,29 @@ void DojoEditorPlugin::_enter_tree()
         return;
     }
     init_config();
-    add_tool_menu_item("Reset Dojo ProjectSettings to default", callable_mp(this, &DojoEditorPlugin::reset_project_settings));
-    add_tool_menu_item("Print godot-dojo Version", callable_mp(this, &DojoEditorPlugin::print_version));
+
+    PopupMenu* menu = memnew(PopupMenu);
+    menu->set_name("DojoToolsMenu");
+    menu->add_item("Reset Dojo ProjectSettings to default", 0);
+    menu->add_item("Print godot-dojo Version", 1);
+    menu->add_item("Check for Updates", 2);
+    menu->connect("id_pressed", callable_mp(this, &DojoEditorPlugin::_on_menu_item_pressed));
+
+    add_tool_submenu_item("Godot Dojo Tools", menu);
+    set_meta("DojoToolsMenu", menu);
     Logger::success_extra("GodotDojo", _get_plugin_version());
 }
 
 void DojoEditorPlugin::_exit_tree()
 {
     Logger::success_extra("DojoEditorPlugin", "Exiting Tree");
-    remove_tool_menu_item("Reset Dojo ProjectSettings to default");
-    remove_tool_menu_item("Print godot-dojo Version");
+    remove_tool_menu_item("Godot Dojo Tools");
+    if (has_meta("DojoToolsMenu")) {
+        if (Node* menu = Object::cast_to<Node>(get_meta("DojoToolsMenu"))) {
+            menu->queue_free();
+        }
+        remove_meta("DojoToolsMenu");
+    }
     Logger::success_extra("DojoEditorPlugin", "Tree exited");
 }
 
@@ -116,4 +135,111 @@ void DojoEditorPlugin::set_setting(const String& setting, const Variant& value, 
     {
         settings->save();
     }
+}
+
+void DojoEditorPlugin::_on_menu_item_pressed(int id)
+{
+    switch (id)
+    {
+        case 0:
+            reset_project_settings();
+            break;
+        case 1:
+            print_version();
+            break;
+        case 2:
+            check_for_updates();
+            break;
+    }
+}
+
+void DojoEditorPlugin::check_for_updates()
+{
+    HTTPRequest* http = memnew(HTTPRequest);
+    add_child(http);
+    http->connect("request_completed", callable_mp(this, &DojoEditorPlugin::_on_release_request_completed).bind(http));
+    
+    PackedStringArray headers;
+    headers.push_back("User-Agent: GodotDojo");
+    http->request("https://api.github.com/repos/lonewolftechnology/godot-dojo/releases", headers);
+    Logger::info("Checking for updates...");
+}
+
+void DojoEditorPlugin::_on_release_request_completed(int result, int response_code, const PackedStringArray& headers, const PackedByteArray& body, HTTPRequest* http)
+{
+    if (result != HTTPRequest::RESULT_SUCCESS)
+    {
+        Logger::error("Failed to check for updates. Request failed.");
+        http->queue_free();
+        return;
+    }
+
+    if (response_code != 200)
+    {
+        Logger::error("Failed to check for updates. Response code:", response_code);
+        http->queue_free();
+        return;
+    }
+
+    Ref<JSON> json;
+    json.instantiate();
+    Error err = json->parse(body.get_string_from_utf8());
+    if (err != OK)
+    {
+        Logger::error("Failed to parse update response.");
+        http->queue_free();
+        return;
+    }
+
+    Array releases = json->get_data();
+    if (!releases.is_empty())
+    {
+        Dictionary latest_release = releases[0];
+        String latest_version = latest_release.get("tag_name", "Unknown");
+        String html_url = latest_release.get("html_url", "");
+        String published_at = latest_release.get("published_at", "Unknown Date");
+        String body_md = latest_release.get("body", "");
+        bool is_prerelease = latest_release.get("prerelease", false);
+        
+        // Conversión simple de Markdown a BBCode
+        Ref<RegEx> regex;
+        regex.instantiate();
+
+        // Bloques de código ``` ... ```
+        regex->compile("(?s)```(.*?)```");
+        body_md = regex->sub(body_md, "[code]$1[/code]", true);
+
+        // Código en línea ` ... `
+        regex->compile("`([^`]+)`");
+        body_md = regex->sub(body_md, "[code]$1[/code]", true);
+
+        // Negrita ** ... **
+        regex->compile("\\*\\*(.*?)\\*\\*");
+        body_md = regex->sub(body_md, "[b]$1[/b]", true);
+
+        // Encabezados # ...
+        regex->compile("(?m)^#+\\s+(.*)$");
+        body_md = regex->sub(body_md, "[b][font_size=10]$1[/font_size][/b]", true);
+
+        // Enlaces texto
+        regex->compile("\\[(.*?)\\]\\((.*?)\\)");
+        body_md = regex->sub(body_md, "[url=$2]$1[/url]", true);
+
+        // Listas - ...
+        regex->compile("(?m)^\\s*-\\s+(.*)$");
+        body_md = regex->sub(body_md, " • $1", true);
+
+        String output = "\n[b][color=green]GodotDojo Update Check[/color][/b]\n";
+        output += "[b]Release Notes:[/b]\n" + body_md + "\n";
+        output += "[b]Version:[/b] " + latest_version + (is_prerelease ? " [color=yellow](Pre-release)[/color]" : "") + "\n";
+        output += "[b]Date:[/b] " + published_at + "\n";
+        output += "[b]URL:[/b] [url]" + html_url + "[/url]\n";
+
+        UtilityFunctions::print_rich(output);
+        
+        String current_version = _get_plugin_version();
+        Logger::info("Current version:", current_version);
+    }
+    
+    http->queue_free();
 }
