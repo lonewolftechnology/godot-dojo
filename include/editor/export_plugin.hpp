4e07b5131ba64886b18240b7209a318d348561e2
Daniel Rodriguez Aires <dannroda@gmail.com>
2026-03-22 22:33:49 -0300

    web: added editor_export plugin that autoenables extensions support and injects dojo initialization

--- include/editor/export_plugin.hpp
+++ include/editor/export_plugin.hpp
@@ -18,13 +18,13 @@ public:
     void _export_begin(const PackedStringArray &p_features, bool p_is_debug, const String &p_path, uint32_t p_flags) override;
     void _export_end() override;
 
-    PackedStringArray _get_export_features(const Ref<EditorExportPlatform> &p_platform, bool p_debug) const override;
     Dictionary _get_export_options_overrides(const Ref<EditorExportPlatform> &p_platform) const override;
     TypedArray<Dictionary> _get_export_options(const Ref<EditorExportPlatform> &p_platform) const override;
     bool _should_update_export_options(const Ref<EditorExportPlatform> &p_platform) const override;
 
     String _get_name() const override { return _plugin_name; }
 
+    static String _get_install_path();
 
 protected:
     static void _bind_methods();
--- src/editor/export_plugin.cpp
+++ src/editor/export_plugin.cpp
@@ -1,56 +1,130 @@
 #include "editor/export_plugin.hpp"
 
 #include <godot_cpp/classes/editor_export_platform_web.hpp>
+#include <godot_cpp/classes/gd_extension_manager.hpp>
 
+#include "godot_cpp/classes/dir_access.hpp"
+#include "godot_cpp/classes/file_access.hpp"
 #include "tools/logger.hpp"
 
-bool GodotDojoExportPlugin::_supports_platform(const Ref<EditorExportPlatform> &p_platform) const {
-    if (p_platform->get_os_name() == "Web"){
+bool GodotDojoExportPlugin::_supports_platform(const Ref<EditorExportPlatform>& p_platform) const
+{
+    if (p_platform->get_os_name() == "Web")
+    {
         return true;
     }
     return false;
 }
 
-void GodotDojoExportPlugin::_export_begin(const PackedStringArray &p_features, bool p_is_debug, const String &p_path,
-    uint32_t p_flags) {
-    Logger::debug_extra("Web BEGIN", p_features, p_is_debug, p_path, p_flags);
-}
+void copy_dir_recursive(const String& from_path, const String& to_path)
+{
+    Ref<DirAccess> dir = DirAccess::open(from_path);
+    if (dir.is_valid())
+    {
+        DirAccess::make_dir_recursive_absolute(to_path);
+        dir->list_dir_begin();
+        String file_name = dir->get_next();
+        while (file_name != "")
+        {
+            if (!file_name.begins_with("."))
+            {
+                String src = vformat("%s/%s", from_path, file_name);
+                String dest = vformat("%s/%s", to_path, file_name);
 
-void GodotDojoExportPlugin::_export_end() {
-    Logger::debug_extra("Web END", "GINISHED");
-}
-
-PackedStringArray GodotDojoExportPlugin::_get_export_features(const Ref<EditorExportPlatform> &p_platform,
-    bool p_debug) const {
-    Logger::debug_extra("Web FEATURES", p_platform, p_debug);
-    if (p_platform->get_os_name() == "Web") {
-        Ref<EditorExportPlatformWeb>
+                if (dir->current_is_dir())
+                {
+                    copy_dir_recursive(src, dest);
+                }
+                else
+                {
+                    String global_src = ProjectSettings::get_singleton()->globalize_path(src);
+                    DirAccess::copy_absolute(global_src, dest);
+                }
+            }
+            file_name = dir->get_next();
+        }
     }
-    Logger::debug_extra("Web Extension", p_platform->get_option("variant/extension_support"));
-    return {
-        Array::make(
-            "variant/extensions_support"
-            )
-    };
+    else
+    {
+        Logger::warning("Origin not found ", from_path);
+    }
 }
 
-Dictionary GodotDojoExportPlugin::_get_export_options_overrides(const Ref<EditorExportPlatform> &p_platform) const {
+void GodotDojoExportPlugin::_export_begin(const PackedStringArray& p_features, bool p_is_debug, const String& p_path,
+                                          uint32_t p_flags)
+{
+    if (p_features.has("web"))
+    {
+        Logger::debug_extra("Web BEGIN", p_features, p_is_debug, p_path, p_flags, _get_install_path());
+        String dojo_path = p_path.get_base_dir();
+        copy_dir_recursive(_get_install_path().path_join("web/dojo"), dojo_path);
+    }
+}
+
+
+void GodotDojoExportPlugin::_export_end()
+{
+    Logger::debug_extra("Export", "END-FINISHED");
+}
+
+Dictionary GodotDojoExportPlugin::_get_export_options_overrides(const Ref<EditorExportPlatform>& p_platform) const
+{
     if (p_platform->get_os_name() == "Web")
-    {Logger::debug_extra("Web OPTIONS OVERRIDES", p_platform);}
+    {
+        Logger::debug_extra("Web OPTIONS OVERRIDES", p_platform);
+        Logger::debug_extra("Web Extension", p_platform->get("variant/extensions_support"));
+        Dictionary _override = {};
+        _override["variant/extensions_support"] = true;
+        _override["html/head_include"] = R"(
+            <script type="module">
+                import init, * as dojo from './godot_dojo_core.js';
+            	console.log("Initializing WASM...");
+            	await init();
+            	window.dojo = dojo;
+
+            </script>
+        )";
+        return _override;
+    }
     return {};
 }
 
-TypedArray<Dictionary> GodotDojoExportPlugin::_get_export_options(const Ref<EditorExportPlatform> &p_platform) const {
-    // if (p_platform->get_os_name() == "Web")
-    // {Logger::debug_extra("Web OPTIONS", p_platform);}
+TypedArray<Dictionary> GodotDojoExportPlugin::_get_export_options(const Ref<EditorExportPlatform>& p_platform) const
+{
+    if (p_platform->get_os_name() == "Web")
+    {
+        // Logger::debug_extra("Web OPTIONS", p_platform);
+        // Logger::debug_extra("Web Extension", p_platform->get("variant/extension_support"));
+    }
     return {};
 }
 
-bool GodotDojoExportPlugin::_should_update_export_options(const Ref<EditorExportPlatform> &p_platform) const {
-    // if (p_platform->get_os_name() == "Web")
-    // {Logger::debug_extra("Web OPTIONS SHOULD", p_platform);}
+bool GodotDojoExportPlugin::_should_update_export_options(const Ref<EditorExportPlatform>& p_platform) const
+{
+    if (p_platform->get_os_name() == "Web")
+    {
+        // Logger::debug_extra("Web OPTIONS SHOULD", p_platform);
+        // Logger::debug_extra("Web Extension", p_platform->get("variant/extension_support"));
+    }
     return false;
 }
 
-void GodotDojoExportPlugin::_bind_methods() {
+String GodotDojoExportPlugin::_get_install_path()
+{
+    String _source_path;
+    PackedStringArray loaded_exts = GDExtensionManager::get_singleton()->get_loaded_extensions();
+    for (const String& ext : loaded_exts)
+    {
+        Logger::debug_extra("Extension", ext);
+        if (ext.ends_with("godot-dojo.gdextension"))
+        {
+            return ext.get_base_dir();
+        }
+    }
+    return _source_path;
+}
+
+void GodotDojoExportPlugin::_bind_methods()
+{
+    ClassDB::bind_static_method(get_class_static(),D_METHOD("_get_install_path"), &GodotDojoExportPlugin::_get_install_path);
 }
